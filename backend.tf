0c0707f5419b9ab589f2b43f1145db32878e8a86
RaytraceSG <azmi.maniku@gmail.com>
2024-09-21 12:56:27 +0800

    update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,4 +4,5 @@ terraform {
     key    = "azmi1-ec2-bluegreen.tfstate" # Name of your tfstate file
     region = "us-east-1"                   # Terraform State bucket region
   }
+  required_version = "1.9.5"
 }
