c5f536e73bac12aa522ce73714315229795c3670
RaytraceSG <azmi.maniku@gmail.com>
2024-09-21 12:15:22 +0800

    add tf files

---
+++ backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce7-tfstate"               # Terraform State bucket name
+    key    = "azmi1-ec2-bluegreen.tfstate"  # Name of your tfstate file
+    region = "us-east-1"                 # Terraform State bucket region
+  }
+}
